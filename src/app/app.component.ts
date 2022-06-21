45f0ec767421d5100b1f18935472c5a3d5c1b8f9
Deep1218 <deeppatel18dec@gmail.com>
2022-06-21 18:54:13 +0530

    [init]

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
 
+import {
+  faHouseChimney,
+  faRightFromBracket,
+  faMagnifyingGlass,
+  faPaperPlane,
+  faUser,
+  faGear,
+} from '@fortawesome/free-solid-svg-icons';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'menestagram';
+  faHouseChimney = faHouseChimney;
+  faMagnifyingGlass = faMagnifyingGlass;
+  faPaperPlane = faPaperPlane;
+  faUser = faUser;
+  faGear = faGear;
+  faRightFromBracket = faRightFromBracket;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FeedCardComponent } from './components/feed-card/feed-card.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, FeedCardComponent],
+  imports: [BrowserModule, AppRoutingModule, FontAwesomeModule, NgbModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
