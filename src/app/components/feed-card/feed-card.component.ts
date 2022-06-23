c4432b534b30135a2e0e09d66b7fbd9065250625
Deep1218 <deeppatel18dec@gmail.com>
2022-06-23 19:09:37 +0530

    [feature] feed card started

--- src/app/components/feed-card/feed-card.component.ts
+++ src/app/components/feed-card/feed-card.component.ts
@@ -1,15 +1,43 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  faEllipsisVertical,
+  faHeart as faHeartSolid,
+  faComment as faCommentSolid,
+  faPaperPlane as faPaperPlaneSolid,
+  faBookmark as faBookmarkSolid,
+  faCommenting,
+} from '@fortawesome/free-solid-svg-icons';
+
+import {
+  faHeart,
+  faComment,
+  faPaperPlane,
+  faBookmark,
+} from '@fortawesome/free-regular-svg-icons';
 
 @Component({
   selector: 'app-feed-card',
   templateUrl: './feed-card.component.html',
-  styleUrls: ['./feed-card.component.css']
+  styleUrls: ['./feed-card.component.css'],
 })
 export class FeedCardComponent implements OnInit {
+  hoverOn = false;
+  faEllipsisV = faEllipsisVertical;
+  faPaperclip = faCommenting;
 
-  constructor() { }
+  faHeartSolid = faHeartSolid;
+  faHeart = faHeart;
 
-  ngOnInit(): void {
-  }
+  faCommentSolid = faCommentSolid;
+  faComment = faComment;
 
+  faPaperPlaneSolid = faPaperPlaneSolid;
+  faPaperPlane = faPaperPlane;
+
+  faBookmarkSolid = faBookmarkSolid;
+  faBookmark = faBookmark;
+
+  constructor() {}
+
+  ngOnInit(): void {}
 }
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,14 +1,26 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SlickCarouselModule } from 'ngx-slick-carousel';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
 import { StoryCardComponent } from '../components/story-card/story-card.component';
 import { StoryComponent } from '../components/story/story.component';
+import { FeedCardComponent } from '../components/feed-card/feed-card.component';
 
 @NgModule({
-  declarations: [HomeComponent, StoryCardComponent, StoryComponent],
-  imports: [CommonModule, HomeRoutingModule, SlickCarouselModule],
+  declarations: [
+    HomeComponent,
+    StoryCardComponent,
+    StoryComponent,
+    FeedCardComponent,
+  ],
+  imports: [
+    CommonModule,
+    HomeRoutingModule,
+    SlickCarouselModule,
+    FontAwesomeModule,
+  ],
 })
 export class HomeModule {}
