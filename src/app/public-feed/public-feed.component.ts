b4b1e27799d596aa32be5b448621809e69c63415
Deep1218 <deeppatel18dec@gmail.com>
2022-06-27 19:03:53 +0530

    [feature] suggested card

--- src/app/public-feed/public-feed.component.ts
+++ src/app/public-feed/public-feed.component.ts
@@ -3,13 +3,68 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-public-feed',
   templateUrl: './public-feed.component.html',
-  styleUrls: ['./public-feed.component.css']
+  styleUrls: ['./public-feed.component.css'],
 })
 export class PublicFeedComponent implements OnInit {
+  slideConfig = {
+    arrows: false,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 2,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
+  data = [
+    {
+      follow: 0,
+      userPic: 'assets/img/download2.jpg',
+      username: 'the_unknow',
+      following: [{ username: 'the_deeppatel' }, { username: 'patel_deep' }],
+    },
+    {
+      follow: 0,
+      userPic: 'assets/img/download2.jpg',
+      username: 'the_unknow',
+      following: [{ username: 'the_deeppatel' }, { username: 'patel_deep' }],
+    },
+    {
+      follow: 0,
+      userPic: 'assets/img/download2.jpg',
+      username: 'the_unknow',
+      following: [{ username: 'the_deeppatel' }, { username: 'patel_deep' }],
+    },
+    {
+      follow: 0,
+      userPic: 'assets/img/download2.jpg',
+      username: 'the_unknow',
+      following: [{ username: 'the_deeppatel' }, { username: 'patel_deep' }],
+    },
+  ];
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
--- src/app/public-feed/public-feed.module.ts
+++ src/app/public-feed/public-feed.module.ts
@@ -1,19 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SlickCarouselModule } from 'ngx-slick-carousel';
 
 import { PublicFeedRoutingModule } from './public-feed-routing.module';
 import { PublicFeedComponent } from './public-feed.component';
 import { SearchComponent } from '../components/search/search.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { ReactiveFormsModule } from '@angular/forms';
+import { SuggestionCardComponent } from '../components/suggestion-card/suggestion-card.component';
 
 @NgModule({
-  declarations: [PublicFeedComponent, SearchComponent],
+  declarations: [PublicFeedComponent, SearchComponent, SuggestionCardComponent],
   imports: [
     CommonModule,
     PublicFeedRoutingModule,
     FontAwesomeModule,
     ReactiveFormsModule,
+    SlickCarouselModule,
   ],
 })
 export class PublicFeedModule {}
