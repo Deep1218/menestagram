21f804163201f53fa0c7b88a2ad9f0585c5e1b2d
Deep1218 <deeppatel18dec@gmail.com>
2022-06-28 13:04:51 +0530

    [init]

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,7 +6,18 @@ const routes: Routes = [
     path: 'home',
     loadChildren: () => import('./home/home.module').then((m) => m.HomeModule),
   },
-  { path: 'publicFeed', loadChildren: () => import('./public-feed/public-feed.module').then(m => m.PublicFeedModule) },
+  {
+    path: 'publicFeed',
+    loadChildren: () =>
+      import('./public-feed/public-feed.module').then(
+        (m) => m.PublicFeedModule
+      ),
+  },
+  {
+    path: 'messages',
+    loadChildren: () =>
+      import('./messages/messages.module').then((m) => m.MessagesModule),
+  },
   { path: '**', redirectTo: 'home', pathMatch: 'full' },
 ];
 
