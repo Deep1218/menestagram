eeedd211d6d01e6ed76b1ecc6d4e500623a37c22
Deep1218 <deeppatel18dec@gmail.com>
2022-07-04 13:03:11 +0530

    [wp] chat list basic ready

--- src/app/messages/messages.component.ts
+++ src/app/messages/messages.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
-
+import {
+  faPhone,
+  faVideo,
+  faCircleInfo,
+} from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-messages',
   templateUrl: './messages.component.html',
-  styleUrls: ['./messages.component.css']
+  styleUrls: ['./messages.component.css'],
 })
 export class MessagesComponent implements OnInit {
+  faPhone = faPhone;
+  faVideo = faVideo;
+  faCircleInfo = faCircleInfo;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
